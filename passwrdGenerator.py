c28abed76d59e9acb1db3f1c0c252c0eb067c4d3
Magpiny <magpinyb@gmail.com>
2021-03-18 21:53:57 +0300

    games folders

--- passwrdGenerator.py
+++ passwrdGenerator.py
@@ -2,11 +2,11 @@ import random
 import string
 
 
-
 def passcode_generator():
     pass_chars = string.ascii_letters + string.digits + string.punctuation
     length = random.randint(8, 25)
     passcode = "".join(random.choices(pass_chars, k=length))
     print(len(passcode))  # monitor the length of my password
+
 
 passcode_generator()
