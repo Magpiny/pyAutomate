359e098bd5cdfa06c714ff4cdc4c76aec1a4e13f
Magpiny <magpinyb@gmail.com>
2021-03-18 21:44:30 +0300

    new

--- lists.py
+++ lists.py
@@ -1,2 +1,15 @@
 # list file
 
+"""
+spam = ['bat', 'rat', 'jog', 'brye']
+print(spam)
+print(spam[1:])
+del spam[2:]
+print(spam)
+"""
+fruits = ['banana', 'oranges', 'apples', 'avocado', 'pawpaw', 'guava']
+
+for frut in range(len(fruits)):
+    print(f"I like {fruits[frut]} ")
+
+
--- passwrdGenerator.py
+++ passwrdGenerator.py
@@ -1,9 +1,12 @@
 import random
 import string
 
-pass_chars = string.ascii_letters + string.digits + string.punctuation
-length = random.randint(8, 16)
-passcode = "".join(random.choices(pass_chars, k=length))
-print(passcode)
-print(len(passcode))
 
+
+def passcode_generator():
+    pass_chars = string.ascii_letters + string.digits + string.punctuation
+    length = random.randint(8, 25)
+    passcode = "".join(random.choices(pass_chars, k=length))
+    print(len(passcode))  # monitor the length of my password
+
+passcode_generator()
